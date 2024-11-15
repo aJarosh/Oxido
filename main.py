34e4d1e697b3fe607e804317e989be039d5d0fb1
aJarosh <aarosz20@gmail.com>
2024-11-15 13:40:06 +0100

    changed except in get_file and checking if content in save is correct

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ class GetFileRequest():
         try:
             response = requests.get(url)
             response.raise_for_status()
+            return response.content
         except requests.exceptions.HTTPError as e:
-            print (e.response.text)
-        return response.content
+            raise SystemExit(e)
     
 class SaveFile():
     def save(file_path, content):
-        try:
-            with open(file_path, 'wb') as file:
-                file.write(content)
-            print('Succesfully saved as: 'f'{file_path}')
-        except IOError as e:
-            print('Error occured during saving the file: {e}')
+        if content:
+            try:
+                with open(file_path, 'wb') as file:
+                    file.write(content)
+                print('Succesfully saved as: 'f'{file_path}')
+            except IOError as e:
+                print('Error occured during saving the file: 'f'{e}')
+        else:
+            print('Content is incorrect')
 
 
 url = "https://cdn.oxido.pl/hr/Zadanie%20dla%20JJunior%20AI%20Developera%20-%20tresc%20artykulu.txt"
+
 content_file = GetFileRequest.get_file(url)
 file_path = "artykul.txt"
 SaveFile.save(file_path, content_file)
