bc02c4777babde840c8468192f36b269c2ec8257
aJarosh <aarosz20@gmail.com>
2024-11-15 13:32:05 +0100

    creation of SaveFile class and artykul.txt with script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 
-class GetFile():
+class GetFileRequest():
     def get_file(url):
         try:
             response = requests.get(url)
@@ -10,6 +10,17 @@ class GetFile():
             print (e.response.text)
         return response.content
     
+class SaveFile():
+    def save(file_path, content):
+        try:
+            with open(file_path, 'wb') as file:
+                file.write(content)
+            print('Succesfully saved as: 'f'{file_path}')
+        except IOError as e:
+            print('Error occured during saving the file: {e}')
+
 
 url = "https://cdn.oxido.pl/hr/Zadanie%20dla%20JJunior%20AI%20Developera%20-%20tresc%20artykulu.txt"
-print(GetFile.get_file(url))
+content_file = GetFileRequest.get_file(url)
+file_path = "artykul.txt"
+SaveFile.save(file_path, content_file)
