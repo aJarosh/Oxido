0fe3f365f0caff8dd11e2f9ee3c9e03789fb09af
aJarosh <aarosz20@gmail.com>
2024-11-15 21:28:47 +0100

    creation of calss SaveFileTXT and HTML

--- main.py
+++ main.py
@@ -10,17 +10,34 @@ class GetFileRequest():
         except requests.exceptions.HTTPError as e:
             raise SystemExit(e)
     
-class SaveFile():
-    def save(file_path, content):
-        if content:
+class SaveFileTXT():
+    def __init__(self, file_path, content):
+        self.file_path = file_path
+        self._content = content
+
+    def save(self):
+        if self._content:
             try:
-                with open(file_path, 'wb') as file:
-                    file.write(content)
-                print('Succesfully saved as: 'f'{file_path}')
+                with open(self.file_path, 'wb') as file:
+                    file.write(self._content)
+                print(f"Succesfully save file to: {self.file_path}")
             except IOError as e:
-                print('Error occured during saving the file: 'f'{e}')
+                print(f"Error ocured during saving: {e}")
         else:
             print('Content is incorrect')
+
+class SaveFileHTML:
+    def __init__(self, file_path, content):
+        self.file_path = file_path
+        self._content = content
+
+    def save(self):
+        try:
+            with open(self.file_path, 'w', encoding='utf-8') as file:
+                file.write(self._content)
+            print(f"Succesfully save file to: {self.file_path}")
+        except IOError as e:
+            print(f"Error ocured during saving: {e}")
 
 
 url = "https://cdn.oxido.pl/hr/Zadanie%20dla%20JJunior%20AI%20Developera%20-%20tresc%20artykulu.txt"
